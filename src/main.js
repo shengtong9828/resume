41340716b411da9585ddd0d33aebd7ede87d471c
盛通 <shengtong9828@163.com>
2021-05-13 21:42:08 +0800

    初始化、引用矢量图标

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-import Vue from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import myIcon from '@/components/common/my-icon.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const vue = createApp(App);
+vue.component('my-icon', myIcon);
+vue.use(router).use(store).mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-icon id="icon-chart-bar" color="text-primary" size="50"></my-icon>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    return {
+    }
   }
 }
 </script>
